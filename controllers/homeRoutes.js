df407eac41d0c68583e2b2f83176fd8157e2284b
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-25 21:00:36 -0600

    fixing homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Goals, TimeCapsule, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const goalsData = await Goals.findAll({
       include: [
         {
           model: User,
@@ -15,11 +15,11 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const goals = projectData.map((goal) => goal.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      goals, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
