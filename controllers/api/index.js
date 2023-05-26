f1343f5739b011cb9dd291bba2ab716a0445e901
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-25 20:34:59 -0600

    fixed typo on timecapsule router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const GoalRoutes = require('./goalRoutes');
-const timeCapsule = require('./timeCapsule)')
+const timeCapsule = require('./timeCapsule')
 
 
 router.use('/users', userRoutes);
