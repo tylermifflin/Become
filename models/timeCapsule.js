ce3b49ffe3a958f740aaf419cbf43d6a5683d348
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-25 19:34:40 -0600

    used TimeCapsule.init and set up id key as integer, not null primary key and autoincrement

--- models/timeCapsule.js
+++ models/timeCapsule.js
@@ -2,3 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class TimeCapsule extends Model {}
+
+TimeCapsule.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,  
+            primaryKey: true,
+            autoIncrement: true,
+        },
