61ecd52eb74af0e0f2a45c721db463b555905c1e
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-30 19:29:48 -0600

    setting up quotes.js to work with quotes.handlebars

---
+++ public/js/quotes.js
@@ -0,0 +1,4 @@
+// add event listener to new quote button to refresh page
+const container = document.getElementById('container');
+const mountainbackground = document.getElementById('mountain-background');
+const forestbackground = document.getElementById('forest-background');
