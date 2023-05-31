74480d39ac022741bd0926e54500cbe363b19980
Connor McGrew <connormcgrew003@gmail.com>
2023-05-30 19:05:24 -0600

    making codedry

--- controllers/api/timeCapsule.js
+++ controllers/api/timeCapsule.js
@@ -2,18 +2,6 @@ const router = require('express').Router();
 const { TimeCapsule } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    const TimeCapsuleData = await TimeCapsule.findAll({
-      include: [{ model: TimeCapsule }],
-    });
-    res.status(200).json(goalsData);
-  }
-  catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.post('/', withAuth, async (req, res) => {
   try {
     const newTimeCapsule = await TimeCapsule.create({
@@ -27,6 +15,26 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+
+  try {
+    const TimeCapsuleData = await TimeCapsule.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!TimeCapsuleData) {
+      res.status(404).json({ message: 'No capsule found with this id!' });
+      return;
+    }
+    res.status(200).json(TimeCapsuleData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const TimeCapsuleData = await TimeCapsule.destroy({
@@ -37,7 +45,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!TimeCapsuleData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+      res.status(404).json({ message: 'No capsule found with this id!' });
       return;
     }
 
