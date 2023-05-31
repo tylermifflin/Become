61cfe8008ab4acb5f59989d84fa329d83b422d72
evermuniz <evermuniz@outlook.com>
2023-05-30 19:36:14 -0600

    dashboard route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,6 +12,24 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Goals, TimeCapsule, User }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/goals', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
