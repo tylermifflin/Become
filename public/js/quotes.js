b24eefefaac2cb52d156d425d48dd7231c751d44
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-30 19:31:15 -0600

    set up function to get a new quote

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -1,4 +1,11 @@
-// add event listener to new quote button to refresh page
+// creating variables to change the background image
 const container = document.getElementById('container');
 const mountainbackground = document.getElementById('mountain-background');
 const forestbackground = document.getElementById('forest-background');
+
+// funtion to get a new quote
+const newquotehandler = async (event) => {
+    event.preventDefault();
+    // refresh page
+    document.location.replace('/quotes');
+}
